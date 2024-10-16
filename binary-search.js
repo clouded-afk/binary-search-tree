41dd25183dd51f11e9d58541fd4c91d4d4d9cfb8
Josh <160084551+clouded-afk@users.noreply.github.com>
2024-10-16 16:08:14 -0400

    Add function to print tree to the console

--- binary-search.js
+++ binary-search.js
@@ -13,12 +13,12 @@ class Tree {
         this.root = this.buildTree(array)
     }
 
-    buildTree(array) {
+    buildTree(array, startingPosition, endingPosition) {
         let removedDuplicatesArray = [...new Set(array)]
         let sortedArray = mergeSort(removedDuplicatesArray)
         
-        const startingPosition = 0
-        const endingPosition = sortedArray.length - 1
+        startingPosition = 0
+        endingPosition = sortedArray.length - 1
 
         if (startingPosition > endingPosition) {
             return null
@@ -31,8 +31,22 @@ class Tree {
     }
 }
 
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      prettyPrint(node.rightNode, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      prettyPrint(node.leftNode, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+  };
+
 const testArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
 
 const treeOne = new Tree(testArray)
 
-treeOne.buildTree(testArray)
+console.log(treeOne.root)
+prettyPrint(treeOne.root)
