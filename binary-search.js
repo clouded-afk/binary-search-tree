77df7a1c6933dba008beee92dbc0e746074a74eb
Josh <160084551+clouded-afk@users.noreply.github.com>
2024-10-16 14:07:11 -0400

    Import mergesort function

--- binary-search.js
+++ binary-search.js
@@ -1,3 +1,5 @@
+import { mergeSort } from "./mergesort.js"
+
 class Node {
     constructor(data) {
         this.data = data
@@ -8,6 +10,24 @@ class Node {
 
 class Tree {
     constructor(array) {
-        this.root = null
+        this.root = this.buildTree(array)
+    }
+
+    buildTree(array) {
+        let startingPosition = 0
+        let endingPosition = array.length - 1
+        let middlePosition = (startingPosition + endingPosition)/2
+
+        const root = array[middlePosition]
+
+        return root
     }
 }
+
+const testArray = mergeSort([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+
+console.log(testArray)
+
+const treeOne = new Tree(testArray)
+
+console.log(treeOne.root)
