47eb9303949366a88b29162c864cc6f555bf2cb5
Josh <160084551+clouded-afk@users.noreply.github.com>
2024-10-16 14:53:47 -0400

    Set root of the tree in the build tree function

--- binary-search.js
+++ binary-search.js
@@ -16,15 +16,19 @@ class Tree {
     buildTree(array) {
         let startingPosition = 0
         let endingPosition = array.length - 1
-        let middlePosition = (startingPosition + endingPosition)/2
 
-        const root = array[middlePosition]
+        if (startingPosition > endingPosition) {
+            return null
+        }
+
+        let middlePosition = (startingPosition + endingPosition)/2
+        const root = new Node(array[middlePosition])
 
         return root
     }
 }
 
-const testArray = mergeSort([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+const testArray = mergeSort([1, 7, 23, 8, 9, 4, 3, 5, 67, 6345, 324])
 
 console.log(testArray)
 
