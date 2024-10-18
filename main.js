2e8f326a9aed4f23dfd06e2791b249a4b1d9682b
Josh <160084551+clouded-afk@users.noreply.github.com>
2024-10-18 16:55:31 -0400

    Add statements to test the bst

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import { Tree } from "./binary-search.js";
 
-const prettyPrint = (node, prefix = "", isLeft = true) => {
+function prettyPrint(node, prefix = "", isLeft = true) {
     if (node === null) {
       return;
     }
@@ -13,8 +13,72 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
     }
 };
 
+let orderArray = []
+
+function printItems(node) {
+    orderArray.push(node.data)
+}
+
 const randomNumberArray = Array.from({length: 14}, () => Math.floor(Math.random() * 100))
 
 const randomNumTree = new Tree(randomNumberArray)
 
+// Testing for Original Tree
+
+console.log('--------------------Original Tree---------------------------')
+
 prettyPrint(randomNumTree.root)
+
+console.log(`\nTree Balanced? ${randomNumTree.isBalanced(randomNumTree.root)}`)
+
+randomNumTree.levelOrder(printItems)
+console.log(`Level-Order: ${orderArray}`)
+orderArray = []
+
+randomNumTree.preOrder(printItems)
+console.log(`Pre-Order: ${orderArray}`)
+orderArray = []
+
+randomNumTree.postOrder(printItems)
+console.log(`Post-Order: ${orderArray}`)
+orderArray = []
+
+randomNumTree.inOrder(printItems)
+console.log(`In-Order: ${orderArray}`)
+orderArray = []
+
+// Testing After Insertion
+
+console.log('\n--------------------Original Tree w/ New Nodes----------------------')
+
+randomNumTree.insert(randomNumTree.root, 574)
+randomNumTree.insert(randomNumTree.root, 8945)
+randomNumTree.insert(randomNumTree.root, 243)
+randomNumTree.insert(randomNumTree.root, 718)
+
+prettyPrint(randomNumTree.root)
+
+console.log(`\nTree Balanced? ${randomNumTree.isBalanced(randomNumTree.root)}`)
+
+
+console.log('\n--------------------Rebalanced Tree----------------------')
+randomNumTree.rebalance()
+
+prettyPrint(randomNumTree.root)
+console.log(`\nTree Balanced? ${randomNumTree.isBalanced(randomNumTree.root)}`)
+
+randomNumTree.levelOrder(printItems)
+console.log(`Level-Order: ${orderArray}`)
+orderArray = []
+
+randomNumTree.preOrder(printItems)
+console.log(`Pre-Order: ${orderArray}`)
+orderArray = []
+
+randomNumTree.postOrder(printItems)
+console.log(`Post-Order: ${orderArray}`)
+orderArray = []
+
+randomNumTree.inOrder(printItems)
+console.log(`In-Order: ${orderArray}`)
+orderArray = []
