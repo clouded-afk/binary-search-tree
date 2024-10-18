f1c3d75c761dbcbe85461608d69eb8ac02ba534e
Josh <160084551+clouded-afk@users.noreply.github.com>
2024-10-18 16:00:34 -0400

    Add function to print bst

---
+++ main.js
@@ -0,0 +1,20 @@
+import { Tree } from "./binary-search.js";
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      prettyPrint(node.rightNode, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      prettyPrint(node.leftNode, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+};
+
+const randomNumberArray = Array.from({length: 14}, () => Math.floor(Math.random() * 100))
+
+const randomNumTree = new Tree(randomNumberArray)
+
+prettyPrint(randomNumTree.root)
